b455369c6457af464d87557d51432f6c46b5ea96
SteaceP <steacy@live.ca>
2022-10-19 20:34:43 -0400

    extended hello world app

--- _hello-world-web-app/main.go
+++ _hello-world-web-app/main.go
@@ -1,37 +1,60 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 )
 
-func main() {
-	// Hello world, the web server
+const portNumber = ":8081"
 
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		n, err := io.WriteString(w, "Hello, world!\n")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Printf("Number of bytes written: %d \n", n)
-	}
-
-	http.HandleFunc("/", helloHandler)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "This is the home page!\n")
 }
 
-//? doesn't work for some reason, I took the example of go.dev
-//?  and modified it to have the same output
-// func main() {
-// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-// 		n, err := fmt.Fprintf(w, "Hello World")
-// 		if err != nil {
-// 			fmt.Println(err)
-// 		}
-// 		fmt.Printf("Number of bytes written: %d \n", n)
-// 	})
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	fmt.Fprintf(w, "This is the about page and 2 + 2 is %d", sum)
+}
 
-// 	_ = http.ListenAndServe(":8080", nil)
-// }
+// addValues adds two integers and return the sum
+func addValues(x, y int) int {
+	return x + y
+}
+
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(100.00, 10.0)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by 0")
+		return
+	}
+
+	fmt.Printf("%f divided by %f is %f\n", 100.0, 10.0, f)
+}
+
+// divideValues divide two float32 and return an error if divided by 0
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("cannot divide by zero")
+		return 0, err
+	}
+
+	result := x / y
+	return result, nil
+}
+
+// main is the main application function
+func main() {
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
+
+	fmt.Printf("Starting application on port %s\n", portNumber)
+
+	log.Fatal(http.ListenAndServe(portNumber, nil))
+}
