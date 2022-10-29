8aec9c17ef2320be1b1357c21c3621244deb9298
SteaceP <steacy@live.ca>
2022-10-29 14:43:05 -0400

    added CSRF middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,8 +3,8 @@ package main
 import (
 	"net/http"
 
-	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/steacep/hello-world/v2/pkg/config"
 	"github.com/steacep/hello-world/v2/pkg/handlers"
 )
@@ -13,6 +13,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
